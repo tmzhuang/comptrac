d8359b725e0b7234d734dfffacd04968d0968522
alishouman <alishouman@cmail.carleton.ca>
2016-03-03 11:43:12 -0500

    Created models for User, Skill, and UserSkill

---
+++ app/views/skills/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @skill, :id, :name, :category, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :user_skills
+  resources :skills
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'user3#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
