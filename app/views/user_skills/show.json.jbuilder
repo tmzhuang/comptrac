6ee0f40e58943e4d63658794f93aaa64a0b3c772
alishouman <alishouman@cmail.carleton.ca>
2016-03-03 11:43:12 -0500

    Created models for User, Skill, and UserSkill

---
+++ app/views/user_skills/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user_skill, :id, :competence, :created_at, :updated_at
---
+++ db/migrate/20160303162507_create_user_skills.rb
@@ -0,0 +1,9 @@
+class CreateUserSkills < ActiveRecord::Migration
+  def change
+    create_table :user_skills do |t|
+      t.integer :competence
+
+      t.timestamps null: false
+    end
+  end
+end
