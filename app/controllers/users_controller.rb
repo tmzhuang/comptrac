31d9a708f6fa123142838164e0a63d3546993a81
tmzhuang <abstract.wolf@gmail.com>
2016-03-10 01:43:18 -0500

    Can add skills on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :add_skills]
+  before_action :set_skills, only: [:show, :edit, :update, :destroy, :add_skills]
+  before_action :set_all_skills, only: [:new, :add_skills, :edit]
+  before_action :set_user_skills, only: [:new, :add_skills, :edit]
+  prepend_before_action :new_user, only: [:new]
 
   # GET /users
   # GET /users.json
@@ -10,16 +14,10 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
-    @skills = @user.skills
-  end
-
-  def skills
   end
 
   # GET /users/new
   def new
-    @user = User.new
   end
 
   # GET /users/add_skill
@@ -34,6 +32,11 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    params[:skills][:ids].each do |skill|
+      if !skill.empty?
+        @user.user_skills.build(:skill_id => skill)
+      end
+    end
 
     respond_to do |format|
       if @user.save
@@ -74,6 +77,22 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def set_skills
+      @skills = User.find(params[:id]).skills
+    end
+
+    def set_all_skills
+      @all_skills = Skill.all
+    end
+
+    def set_user_skills
+      @user_skills = @user.user_skills.build
+    end
+
+    def new_user
+      @user = User.new
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
