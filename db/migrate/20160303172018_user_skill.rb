50063d180476ca5c48ba29e960bcde46fd6f95ba
tmzhuang <abstract.wolf@gmail.com>
2016-03-10 11:09:00 -0500

    User Skill validations
    Validates the uniqueness of the composite key in UserSkill table.

--- db/migrate/20160303172018_user_skill.rb
+++ db/migrate/20160303172018_user_skill.rb
@@ -1,4 +1,6 @@
 class UserSkill < ActiveRecord::Migration
   def change
+    remove_index :user_skill, :user_id, :skill_id
+    add_index :user_skill, [:user_id, :skill_id], unique: true
   end
 end
