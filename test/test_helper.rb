4e375c885e08884f59585a1251f5bf2566ff2617
tmzhuang <abstract.wolf@gmail.com>
2016-03-11 19:24:44 +0000

    Testing dependency grapher

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,24 +2,48 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'dependency_grapher'
+require 'bundler'
+
+class ActionController::TestCase
+
+  # Add more helper methods to be used by all tests here...
+  #@@logger = DependencyGrapher::Logger.new
+
+  #def setup
+  #puts "in setup"
+  #@@logger.enable
+  #end
+
+  #def teardown
+  #p "in teardown"
+  #@@logger.disable
+  #end
+
+  #Minitest.after_run do 
+  #analyzer = DependencyGrapher::Grapher.new(@@logger.dependencies)
+  ##grapher = DependencyGrapher::Grapher.new(@@logger.dependencies)
+  #end
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-	@@logger = DependencyGrapher::Logger.new
+  @@logger = DependencyGrapher::Logger.new
 
-	def setup
-		@@logger.enable
-	end
+  def setup
+    puts "in setup"
+    @@logger.enable
+  end
 
-	def teardown
-		@@logger.disable
-	end
+  def teardown
+    p "in teardown"
+    @@logger.disable
+  end
 
-	Minitest.after_run do 
+  Minitest.after_run do 
     analyzer = DependencyGrapher::Grapher.new(@@logger.dependencies)
-		#grapher = DependencyGrapher::Grapher.new(@@logger.dependencies)
-	end
+    #grapher = DependencyGrapher::Grapher.new(@@logger.dependencies)
+  end
 end
