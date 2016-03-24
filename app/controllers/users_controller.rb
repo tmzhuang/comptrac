b3f62848340737a62bca18c9464bf7cfe3da36f9
alishouman <alishouman@cmail.carleton.ca>
2016-03-23 20:52:13 -0400

    comeptency added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :add_skills, :finish_signup]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :add_skills]
   before_action :set_skills, only: [:show, :edit, :update, :destroy, :add_skills]
   before_action :set_all_skills, only: [:new, :add_skills, :edit]
   before_action :set_user_skills, only: [:new, :add_skills, :edit]
   prepend_before_action :new_user, only: [:new]
-  before_filter :ensure_signup_complete, only: [:new, :create, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -23,14 +22,7 @@ class UsersController < ApplicationController
 
   # GET /users/add_skill
   def add_skills
-    # @user.skills << Skill.find(params[:skill_id])
-  end
-
-  def add_powers
-
-  end
-
-  def register
+   # @user.skills << Skill.find(params[:skill_id])
   end
 
   # GET /users/1/edit
@@ -54,7 +46,7 @@ class UsersController < ApplicationController
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+     end
     end
   end
 
@@ -66,29 +58,14 @@ class UsersController < ApplicationController
         @user.skills << Skill.find(id)
       end
     end
-
+   
     respond_to do |format|
       if @user.update(user_params)
-        sign_in(@user == current_user ? @user : current_user, :bypass => true)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # GET/PATCH /users/:id/finish_signup
-  def finish_signup
-    # authorize! :update, @user 
-    if request.patch? && params[:user] #&& params[:user][:email]
-      if @user.update(user_params)
-        @user.skip_reconfirmation!
-        sign_in(@user, :bypass => true)
-        redirect_to @user, notice: 'Your profile was successfully updated.'
-      else
-        @show_errors = true
       end
     end
   end
@@ -104,32 +81,33 @@ class UsersController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
-  def set_skills
-    @skills = User.find(params[:id]).skills
-  end
+    def set_skills
+      @skills = @user.skills
+    end
 
-  def set_all_skills
-    @all_skills = Skill.all
-  end
+    def set_all_skills
+      @all_skills = Skill.all
+    end
 
-  def set_user_skills
-    @user_skills = @user.user_skills.build
-  end
+    def set_user_skills
+      @user_skills = @user.user_skills.build
+     # @user_skills = UserSkill.find_by skill_id: 1, user_id: 1
+    end
+    def set_user_competence
+     @user_competence=UserSkill.select("competence").find_by skill_id: 1, user_id: 1
+    end
 
-  def new_user
-    @user = User.new
-  end
+    def new_user
+      @user = User.new
+    end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-    #params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    accessible = [ :name, :email, :id ] # extend with your own params
-    accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
-    params.require(:user).permit(accessible)
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
