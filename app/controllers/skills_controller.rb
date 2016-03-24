06a59ecc293e8a99ceabe9ee773d2a77a845e381
tmzhuang <abstract.wolf@gmail.com>
2016-03-24 04:28:33 +0000

    Fixed skills functionality

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -28,7 +28,7 @@ class SkillsController < ApplicationController
 
     respond_to do |format|
       if @skill.save
-        format.html { redirect_to @skill, notice: 'Skill was successfully created.' }
+        format.html { redirect_to '/skills', notice: 'Skill was successfully created.' }
         format.json { render :show, status: :created, location: @skill }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class SkillsController < ApplicationController
   def update
     respond_to do |format|
       if @skill.update(skill_params)
-        format.html { redirect_to @skill, notice: 'Skill was successfully updated.' }
+        format.html { redirect_to '/skills', notice: 'Skill was successfully updated.' }
         format.json { render :show, status: :ok, location: @skill }
       else
         format.html { render :edit }
