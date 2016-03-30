eaed163fbe0514316ebcc78751895c94bc5b1144
monisind <alishouman@cmail.carleton.ca>
2016-03-30 16:40:37 +0000

    search by skill completed

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'user_skills#index'
 
-  post 'user_skills/search_user'	
-  post 'user_skills/search_skill'	
+  post 'user_skills/search'	
+	
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
