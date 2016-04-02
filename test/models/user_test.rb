42532d9b5f08f646de42df68f95dfa59316b7dd3
tmzhuang <abstract.wolf@gmail.com>
2016-04-01 22:08:01 -0400

    Cleaning change from dependency_grapher

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,4 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    #p "in UserTest the truth"
-    @user = users(:tianming)
-    @skill = skills(:ruby)
-    @user.add_skill(@skill)
-    @user.register(:tianming)
-    assert @user.has_skill?(@skill)
-  end
 end
