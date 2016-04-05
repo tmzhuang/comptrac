3f716ad569e40276ddda41dd499b3abe6345faaf
tmzhuang <abstract.wolf@gmail.com>
2016-04-05 02:18:36 +0000

    Show roles on user profile
    users/show.html.haml now displays roles if they apply. The skills for
    which assessor roles apply are listed.
    
    Additionally, added links to User and Skill index to nav bar in
    application.html.haml.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   skip_after_action :verify_authorized
 
-  def landing
+  def index
     if user_signed_in?
       redirect_to current_user
     end
